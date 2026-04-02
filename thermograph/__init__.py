e0234d17b52dc9619716f930a470e5fac9f58bc4
mwelland <mike@mikewelland.com>
2026-04-02 13:14:52 -0400

    refactor: Remove ZNetThermo class and update interaction handling in ZNet

--- thermograph/__init__.py
+++ thermograph/__init__.py
@@ -1,9 +1,7 @@
-from .model import ZNetThermo
 from .nodes.sgte import SGTENode
 from .constants import KB_EV, KB_J, KB_R, DEFAULT_KB
 
 __all__ = [
-    "ZNetThermo",
     "SGTENode",
     "KB_EV",
     "KB_J", 
